93013d375205a2d937f4db310b67d03131262c2a
KESARA SUNAYANA <kesarasunayana05@gmail.com>
2024-09-01 19:43:57 +0530

    Add files via upload

---
+++ TENSORFLOWPROJECTOUTPUT_AICTE.py
@@ -0,0 +1,53 @@
+Epoch 1/25
+30/30 - 4s - accuracy: 0.5760 - loss: 0.6748 - val_accuracy: 0.6080 - val_loss: 0.6400
+Epoch 2/25
+30/30 - 2s - accuracy: 0.6191 - loss: 0.6390 - val_accuracy: 0.6445 - val_loss: 0.6107
+Epoch 3/25
+30/30 - 2s - accuracy: 0.6435 - loss: 0.6193 - val_accuracy: 0.6497 - val_loss: 0.6006
+Epoch 4/25
+30/30 - 2s - accuracy: 0.6515 - loss: 0.6035 - val_accuracy: 0.6699 - val_loss: 0.5947
+Epoch 5/25
+30/30 - 2s - accuracy: 0.6687 - loss: 0.5899 - val_accuracy: 0.6635 - val_loss: 0.5878
+Epoch 6/25
+30/30 - 2s - accuracy: 0.6724 - loss: 0.5894 - val_accuracy: 0.6663 - val_loss: 0.5834
+Epoch 7/25
+30/30 - 2s - accuracy: 0.6735 - loss: 0.5786 - val_accuracy: 0.6744 - val_loss: 0.5792
+Epoch 8/25
+30/30 - 2s - accuracy: 0.6777 - loss: 0.5749 - val_accuracy: 0.6852 - val_loss: 0.5764
+Epoch 9/25
+30/30 - 2s - accuracy: 0.6758 - loss: 0.5776 - val_accuracy: 0.6821 - val_loss: 0.5730
+Epoch 10/25
+30/30 - 2s - accuracy: 0.6859 - loss: 0.5650 - val_accuracy: 0.6782 - val_loss: 0.5709
+Epoch 11/25
+30/30 - 2s - accuracy: 0.6817 - loss: 0.5745 - val_accuracy: 0.6856 - val_loss: 0.5681
+Epoch 12/25
+30/30 - 2s - accuracy: 0.6851 - loss: 0.5719 - val_accuracy: 0.6825 - val_loss: 0.5666
+Epoch 13/25
+30/30 - 2s - accuracy: 0.6894 - loss: 0.5643 - val_accuracy: 0.6855 - val_loss: 0.5647
+Epoch 14/25
+30/30 - 2s - accuracy: 0.6928 - loss: 0.5607 - val_accuracy: 0.7026 - val_loss: 0.5659
+Epoch 15/25
+30/30 - 2s - accuracy: 0.6962 - loss: 0.5582 - val_accuracy: 0.6945 - val_loss: 0.5614
+Epoch 16/25
+30/30 - 2s - accuracy: 0.6942 - loss: 0.5577 - val_accuracy: 0.6864 - val_loss: 0.5613
+Epoch 17/25
+30/30 - 2s - accuracy: 0.6883 - loss: 0.5593 - val_accuracy: 0.6871 - val_loss: 0.5601
+Epoch 18/25
+30/30 - 2s - accuracy: 0.6973 - loss: 0.5535 - val_accuracy: 0.7089 - val_loss: 0.5623
+Epoch 19/25
+30/30 - 2s - accuracy: 0.6948 - loss: 0.5599 - val_accuracy: 0.7013 - val_loss: 0.5585
+Epoch 20/25
+30/30 - 2s - accuracy: 0.6988 - loss: 0.5534 - val_accuracy: 0.6872 - val_loss: 0.5577
+Epoch 21/25
+30/30 - 2s - accuracy: 0.6952 - loss: 0.5515 - val_accuracy: 0.7037 - val_loss: 0.5561
+Epoch 22/25
+30/30 - 2s - accuracy: 0.6996 - loss: 0.5519 - val_accuracy: 0.7027 - val_loss: 0.5558
+Epoch 23/25
+30/30 - 2s - accuracy: 0.7050 - loss: 0.5473 - val_accuracy: 0.6998 - val_loss: 0.5545
+Epoch 24/25
+30/30 - 2s - accuracy: 0.6961 - loss: 0.5548 - val_accuracy: 0.6988 - val_loss: 0.5540
+Epoch 25/25
+30/30 - 2s - accuracy: 0.7019 - loss: 0.5467 - val_accuracy: 0.7028 - val_loss: 0.5535
+49/49 - 2s - accuracy: 0.6972 - loss: 0.5551
+loss: 0.555
+accuracy: 0.697
